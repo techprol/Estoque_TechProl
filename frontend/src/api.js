e4bcee1e9af202b239672d71a0fbe92e08173867
techprol <qualidadetechprolrn@gmail.com>
2025-11-27 09:21:05 -0300

    change for hospedagem

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000";
+const API_URL = "https://estoque-techprol.onrender.com/";
 
 export default axios.create({
     baseURL: API_URL,
